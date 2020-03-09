19f1ec64470cea61c400e55b10b313a3327a5cae
steve0079 <49959235+steve0079@users.noreply.github.com>
2020-03-09 13:25:51 -0700

    added link to phone number and modified home links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <p><a href="mailto:stevem9512@verizon.net">stevem9512@verizon.net</a>  |  562.394.8188</p>
+            <p><a href="mailto:stevem9512@verizon.net">stevem9512@verizon.net</a>  |  <a href="tel:562.394.8188">562.394.8188</a></p>
             <p className="copyright">©2020 Steve Martin Graphics. All rights reserved.</p>
         </div>
     )
